5b24fe361bb29ced60b75145fc672d57db3bdf86
munisekar-py <munisheak@gmail.com>
2025-04-28 19:06:47 +0530

    Updated Password and Backup files

--- q1_password_check.py
+++ q1_password_check.py
@@ -1,25 +1,32 @@
+#In this program am utilizing Built-in function and Built-in string methods without importing other 3rd Party Modules
+
+
+#defining Function
 def check_password_strength(password):
-    feedback = []
+    feedback = []   #Create an empty list for feedback
     
-    #Checks its strength using 5 rules
+    #Checking its strength using 5 rules 
     
     if len(password) < 8:
         feedback.append("Password should be at least 8 characters long.")
+        #Below It loops through each character (c) in the password.
     if not any(c.isupper() for c in password):
         feedback.append("Include at least one uppercase letter.")
     if not any(c.islower() for c in password):
         feedback.append("Include at least one lowercase letter.")
     if not any(c.isdigit() for c in password):
         feedback.append("Include at least one digit.")
+        # Special Character Check
     if not any(c in "!@#$%^&*()-_=+[]{}|;:'\",.<>?/" for c in password):
         feedback.append("Include at least one special character (!@#$%^&* etc.)")
     
+    # Returning Results
     is_strong = len(feedback) == 0
     return is_strong, feedback
 
-# Getting User input
+# Getting Password from  User
 pwd = input("Enter your password to check its strength: ")
-strong, issues = check_password_strength(pwd)
+strong, issues = check_password_strength(pwd)  #Calling the Function
 if strong:
     print("Your password is strong!")
 else:
